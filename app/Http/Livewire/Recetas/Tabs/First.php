e527c20037e8c93f77b46dc37c106b526ec17ce2
Ignacio Gonzalez <101266032+nashgzzz@users.noreply.github.com>
2023-02-15 13:45:53 -0200

    antecedentes

--- app/Http/Livewire/Recetas/Tabs/First.php
+++ app/Http/Livewire/Recetas/Tabs/First.php
@@ -3,14 +3,15 @@
 namespace App\Http\Livewire\Recetas\Tabs;
 
 use Livewire\Component;
-use App\Models\QuizzAnswer;
+use App\Models\Patient;
 
 class First extends Component
 {
     public $respuestas;
 
     public function mount($solicitud){
-        $this->respuestas = QuizzAnswer::where('solicitud_id',$solicitud->id)->get();
+        // $this->respuestas = QuizzAnswer::where('solicitud_id',$solicitud->id)->get();
+        $this->respuestas = Patient::all();
     }
     public function render()
     {
--- resources/views/livewire/recetas/tabs/first.blade.php
+++ resources/views/livewire/recetas/tabs/first.blade.php
@@ -1,18 +1,37 @@
 <div>
-    <div class="grid md:grid-cols-2 md:gap-6 mt-10">
+    <div class="grid grid-cols-1 gap-2">
         @foreach ($respuestas as $key => $respuesta )
             @if($key == 9)
-            <br>
-                <div class="relative z-0 mb-6 w-full group">
-                    <textarea readonly type="text" name="floating_first_name" id="floating_first_name" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required="">{{$respuesta->answer}}</textarea>
-                    <label for="floating_first_name" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 truncate">{{$respuesta->quizz_question->question}}</label>
-                </div>
-            @else
+            
+                    <div class="grid grid-cols-1">
+                        <div class="font-bold"> 1) Medicamentos frecuentes</div>
+                        <input type="text" placeholder ="{{$respuesta->p1}}" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    </div>
+        
+                    <div class="grid grid-cols-1">
+                        <div class="font-bold"> 2) Es alérgico</div>
+                        <input type="text"   placeholder ="{{$respuesta->p2}}" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    </div>
+                  
+                    <div class="grid grid-cols-1">
+                        <div class="font-bold"> 3) Indique patologías previas</div>
+                        <input type="text"   placeholder ="{{$respuesta->p3}}" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    </div>
+                 
+                    <div class="grid grid-cols-1">
+                        <div class="font-bold"> 4) Se ha realizado cirugías </div>
+                        <input type="text"   placeholder ="{{$respuesta->p4}}" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    </div>
+               
+                    <div class="grid grid-cols-1">
+                        <div class="font-bold"> 5) Tiene algunas observaciones</div>
+                        <input type="text"  placeholder ="{{$respuesta->p5}}"  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    </div>
 
-                <div class="relative z-0 mb-6 w-full group">
-                    <input readonly type="text" name="floating_first_name" id="floating_first_name" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required="" value="{{$respuesta->answer}}">
-                    <label for="floating_first_name" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 truncate">{{$respuesta->quizz_question->question}}</label>
-                </div>
+                  
+            @else
+           
+              
             @endif
         @endforeach
     </div>
