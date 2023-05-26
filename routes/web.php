a3ed41087a59fe02c43d28b600a43e2cb2543a92
Juan Andres <javt1981@gmail.com>
2023-05-26 05:11:00 -0400

    v2

--- routes/web.php
+++ routes/web.php
@@ -34,7 +34,7 @@ use App\Http\Livewire\Paciente\ListadoPacientes;
 |
 */
 
-Route::get('storage-link', function(){
+Route::get('storage-link', function () {
     Artisan::call('storage:link');
     return '<h1>Storage link creado</h1>';
 })->middleware('auth');
@@ -54,64 +54,61 @@ Route::get('step-ten',[StepsController::class, 'stepTen'])->name('step.ten');
 Route::get('step-two',[StepsController::class, 'stepTwo'])->name('step.two'); */
 
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
 
-Route::group(['middleware' => ['auth']], function() {
+Route::group(['middleware' => ['auth']], function () {
 
     Route::get('/', function () {
-        $doctores = Doctor::where('state',1)->get();
+        $doctores = Doctor::where('state', 1)->get();
         return view('welcome', compact('doctores'));
     })->name('/');
 
     Route::get('/dashboard', Inicio::class)->name('dashboard');
     Route::get('/', Inicio::class)->name('/');
 
-    Route::post('to-step-two',[StepsController::class, 'toStepTwo'])->name('step.create.one');
+    Route::post('to-step-two', [StepsController::class, 'toStepTwo'])->name('step.create.one');
     Route::middleware('guest')->group(function () {
 
-    Route::get('register/doctor', [RegisteredUserController::class, 'create_doc'])
-                    ->name('register.doc');
+        Route::get('register/doctor', [RegisteredUserController::class, 'create_doc'])
+            ->name('register.doc');
     });
 
     // Route::get('step-one',Steps::class)->middleware(['role:Patient'],['role:Admin'])->name('step.one');
-    Route::get('{paciente}/step-one',Steps::class)->name('step.one');
+    Route::get('{paciente}/step-one', Steps::class)->name('step.one');
 
-    Route::get('/admin',[HomeController::class, 'index'])->name('admin');
+    Route::get('/admin', [HomeController::class, 'index'])->name('admin');
     Route::resource('roles', RoleController::class)->middleware(['role:Admin']);
     Route::resource('users', UserController::class)->middleware(['role:Admin']);
-    Route::get('solicitudes',[SolicitudController::class, 'index'])->name('solicitudes');
-  /*   Route::get('pacientes/show',[PatientController::class, 'show'])->name('pacientes.show');
+    Route::get('solicitudes', [SolicitudController::class, 'index'])->name('solicitudes');
+    /*   Route::get('pacientes/show',[PatientController::class, 'show'])->name('pacientes.show');
     Route::get('pacientes/crear',[PatientController::class, 'create'])->name('pacientes.create');
     Route::get('pacientes/preguntas',[PatientController::class, 'preguntas'])->name('pacientes.preguntas');
     Route::post('pacientes/store',[PatientController::class, 'store'])->name('pacientes.store'); */
     /*     Route::get('doctores',[DoctorController::class, 'index'])->name('doctores'); */
-    Route::get('kine/crear',[DoctorController::class, 'create'])->name('doctor.create');
-    Route::get('kine',[DoctorController::class, 'index'])->name('doctor.index');
-    Route::post('kine/store',[DoctorController::class, 'store'])->name('doctor.store');
+    Route::get('kine/crear', [DoctorController::class, 'create'])->name('doctor.create');
+    Route::get('kine', [DoctorController::class, 'index'])->name('doctor.index');
+    Route::post('kine/store', [DoctorController::class, 'store'])->name('doctor.store');
 
-    Route::get('resumen',[ResumenController::class, 'index'])->name('resumen');
-    Route::get('resumen-show/{res}',[ResumenController::class, 'show'])->name('resumen.show');
-    Route::get('receta',[ResumenController::class, 'receta'])->name('receta.demo');
-    Route::post('crear/receta',[ResumenController::class,'crearReceta'])->name('crear.receta');
-    Route::post('crear/receta',[SesionController::class,'crearSesion'])->name('sesion.crear');
-    
-    Route::get('repetir/{id}/receta',[SolicitudController::class, 'repetir'])->name('repetir.receta');
-    
+    Route::get('resumen', [ResumenController::class, 'index'])->name('resumen');
+    Route::get('resumen-show/{res}', [ResumenController::class, 'show'])->name('resumen.show');
+    Route::get('receta', [ResumenController::class, 'receta'])->name('receta.demo');
+    Route::post('crear/receta', [ResumenController::class, 'crearReceta'])->name('crear.receta');
+    Route::post('crear/receta', [SesionController::class, 'crearSesion'])->name('sesion.crear');
+
+    Route::get('repetir/{id}/receta', [SolicitudController::class, 'repetir'])->name('repetir.receta');
+
     //Livewire full page components
-    Route::get('pacientes',ListadoPacientes::class)->name('pacientes');
-    Route::get('kines',ListadoKines::class)->name('kines');
-    Route::get('solicitudes/{solicitud?}/ver',\App\Http\Livewire\Recetas\Index::class)->name('solicitud.show');
+    Route::get('pacientes', ListadoPacientes::class)->name('pacientes');
+    Route::get('kines', ListadoKines::class)->name('kines');
+    Route::get('solicitudes/{solicitud?}/ver', \App\Http\Livewire\Recetas\Index::class)->name('solicitud.show');
     Route::get('kines/{kine?}/ver/', \App\Http\Livewire\Doctor\Index::class)->name('doctor.show');
 
-    Route::get('ayuda',[VideoController::class, 'index'])->name('ayudaVideo');
-
-    Route::get('eliminar/apoderado/{id?}', \App\Http\Livewire\Patient\Card::class, 'deleteApoderado')->name('eliminar.apoderado');
-
+    Route::get('ayuda', [VideoController::class, 'index'])->name('ayudaVideo');
 });
 
 //Transbank
-Route::post('iniciar-compra',[TransbankController::class, 'iniciarCompra'])->name('iniciar.compra');
-Route::match(array('GET','POST'),'/confirmar-pago', [TransbankController::class, 'confirmar_pago'])->name('confirmar.pago');
+Route::post('iniciar-compra', [TransbankController::class, 'iniciarCompra'])->name('iniciar.compra');
+Route::match(array('GET', 'POST'), '/confirmar-pago', [TransbankController::class, 'confirmar_pago'])->name('confirmar.pago');
 
 Route::get('/pago-ok/{id_transaccion}', [TransbankController::class, 'pago_ok'])->name('pago.ok');
-Route::get('/pago-fallido',[TransbankController::class,'pago_fallido'])->name('pago.fallido');
+Route::get('/pago-fallido', [TransbankController::class, 'pago_fallido'])->name('pago.fallido');
