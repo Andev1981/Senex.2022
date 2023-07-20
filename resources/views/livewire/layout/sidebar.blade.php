15e2819559c174e5f9fc46e1fe9d21de868f8a59
Juan Andres <javt1981@gmail.com>
2023-07-20 01:27:15 -0400

    hide button

--- resources/views/livewire/layout/sidebar.blade.php
+++ resources/views/livewire/layout/sidebar.blade.php
@@ -33,11 +33,11 @@
                     </a>
                 </form>
             </li>
-            <li>
+          <!--   <li>
                 <button wire:click="updateData">
                     Update list
                 </button>
-            </li>
+            </li> -->
         </ul>
         <ul class="pt-5 mt-5 space-y-2 border-t border-gray-200 dark:border-gray-700">
 
