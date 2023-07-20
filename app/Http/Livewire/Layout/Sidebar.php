ad13d0dfdddaa4b5627c7c7a892b457e861c5f48
Juan Andres <javt1981@gmail.com>
2023-07-20 01:19:34 -0400

    update options

--- app/Http/Livewire/Layout/Sidebar.php
+++ app/Http/Livewire/Layout/Sidebar.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire\Layout;
 
+use App\Models\SelectOption;
 use Livewire\Component;
 
 class Sidebar extends Component
@@ -9,5 +10,20 @@ class Sidebar extends Component
     public function render()
     {
         return view('livewire.layout.sidebar');
+    }
+
+    public function updateData(){
+
+        SelectOption::create([
+            'sort_order' => 11,
+            'name' => 'Esposa',
+            'model_type' =>'Keepers',
+        ]);
+        SelectOption::create([
+            'sort_order' => 12,
+            'name' => 'Esposo',
+            'model_type' =>'Keepers',
+        ]);
+
     }
 }
--- resources/views/livewire/layout/sidebar.blade.php
+++ resources/views/livewire/layout/sidebar.blade.php
@@ -1,4 +1,4 @@
-<aside class="fixed top-0 left-0 z-40 w-64 h-screen pt-14 transition-transform -translate-x-full bg-white border-r border-gray-200 md:translate-x-0 dark:bg-gray-800 dark:border-gray-700" aria-label="Sidenav" id="drawer-navigation">
+<aside class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full bg-white border-r border-gray-200 pt-14 md:translate-x-0 dark:bg-gray-800 dark:border-gray-700" aria-label="Sidenav" id="drawer-navigation">
     <div class="h-full px-3 py-5 overflow-y-auto bg-white dark:bg-gray-800">
         <form action="#" method="GET" class="mb-2 md:hidden">
 
@@ -32,6 +32,11 @@
                                     {{ __('Cerrar sesión') }}
                     </a>
                 </form>
+            </li>
+            <li>
+                <button wire:click="updateData">
+                    Update list
+                </button>
             </li>
         </ul>
         <ul class="pt-5 mt-5 space-y-2 border-t border-gray-200 dark:border-gray-700">
