0203f93933811a8c1e9c2f98e69d816a0bfa916c
Andev2000 <javt1981@gmail.com>
2023-08-11 12:11:14 -0400

    opendelpaciente

--- resources/views/livewire/paciente/show/index.blade.php
+++ resources/views/livewire/paciente/show/index.blade.php
@@ -89,7 +89,7 @@
     </div>
 
        <!-- Modal Eliminar -->
-    <div class="{{ $openDelAtencion }} bg-gray-600 bg-opacity-50 overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-modal md:h-full flex">
+    <div class="{{ $openDelPaciente }} bg-gray-600 bg-opacity-50 overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-modal md:h-full flex">
         <div class="relative w-full h-full max-w-2xl p-4 md:h-auto">
             <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
                 <button type="button" wire:click="$set('openDelPaciente','hidden')" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white">
