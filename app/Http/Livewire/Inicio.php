c852367339f0b5611936741b88e665b39804a516
Andev2000 <javt1981@gmail.com>
2023-08-11 12:08:14 -0400

    applu update usere

--- app/Http/Livewire/Inicio.php
+++ app/Http/Livewire/Inicio.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\ApplicationTypeUser;
 use Livewire\Component;
 
 class Inicio extends Component
 {
     public function render()
     {
+        $applycationTypeUser = ApplicationTypeUser::where('price', null)->get();
+
+            foreach($applycationTypeUser as $apply){
+                $apply->price = 0;
+                $apply->save();
+            }
+
         return view('livewire.inicio');
     }
 }
