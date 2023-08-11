bdff87e05edff5420831dced878a1bc61721a839
Andev2000 <javt1981@gmail.com>
2023-08-11 13:08:02 -0400

    fix listado botones

--- resources/views/livewire/paciente/show/index.blade.php
+++ resources/views/livewire/paciente/show/index.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <div>
+    <div class="flex flex-row gap-4">
         <button wire:click="$set('opendetalles', '')" class="flex items-center px-2 py-1 text-sm font-medium text-center text-gray-900 bg-white border border-gray-200 rounded-lg focus:outline-none hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700" type="button">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-4 h-4 mr-2 -ml-0.5">
                 <path d="M12 15a3 3 0 100-6 3 3 0 000 6z"></path>
