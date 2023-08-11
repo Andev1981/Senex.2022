609bd6aa7eed62f1a659aeba79653647b274037e
Andev2000 <javt1981@gmail.com>
2023-08-11 12:12:20 -0400

    ??

--- app/Http/Livewire/Inicio.php
+++ app/Http/Livewire/Inicio.php
@@ -9,12 +9,13 @@ class Inicio extends Component
 {
     public function render()
     {
-        $applycationTypeUser = ApplicationTypeUser::where('price', null)->get();
+        $applycationTypeUser = ApplicationTypeUser::get();
+        dd($applycationTypeUser);
 
-            foreach($applycationTypeUser as $apply){
+           /*  foreach($applycationTypeUser as $apply){
                 $apply->price = 0;
                 $apply->save();
-            }
+            } */
 
         return view('livewire.inicio');
     }
