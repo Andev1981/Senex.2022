31b269fd11b8735f891f18afdee56109baa91a9a
Andev2000 <javt1981@gmail.com>
2023-08-11 13:22:47 -0400

    tabla resumen kines

--- resources/views/livewire/kine/assign-index.blade.php
+++ resources/views/livewire/kine/assign-index.blade.php
@@ -184,127 +184,129 @@
                             </div>
                         </div>
                     </form>
+                    <div class="max-w-screen-xl px-1 mx-auto lg:px-2">
+                    
+                        <div class="relative overflow-hidden bg-white shadow-md dark:bg-gray-800 sm:rounded-lg">
 
-                    <div class="bg-white">
+                            <div class="flex flex-row items-center p-4 bg-slate-200 md:flex-row md:space-y-0 md:space-x-4">
 
-                        <div class="flex items-center justify-between w-full p-2 font-medium text-left text-gray-500 bg-gray-100 border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-800">
+                                <span>Atenciones </span>
+                                <select class="block w-full p-1 ml-4 text-xs text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" wire:model="year">
 
-                            <span>Atenciones </span>
-                            <select class="block w-full p-1 ml-4 text-xs text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" wire:model="year">
+                                    <option selected value="2023">2023</option>
+                                    <option value="2024">2024</option>
+                                    <option value="2025">2025</option>
+                                    <option value="2026">2026</option>
+                                    <option value="2028">2028</option>
+                                    <option value="2029">2029</option>
+                                    <option value="2030">2030</option>
+                                </select>
+                                <select class="block w-full p-1 ml-4 text-xs text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" wire:model="month">
+                                    <option value="01">Enero</option>
+                                    <option value="02">Febrero</option>
+                                    <option value="03">Marzo</option>
+                                    <option value="04">Abril</option>
+                                    <option value="05">Mayo</option>
+                                    <option value="06">Junio</option>
+                                    <option value="07">Julio</option>
+                                    <option value="08">Agosto</option>
+                                    <option value="09">Septiembre</option>
+                                    <option value="10">Octubre</option>
+                                    <option value="11">Noviembre</option>
+                                    <option value="12">Diciembre</option>
+                                </select>
 
-                                <option selected value="2023">2023</option>
-                                <option value="2024">2024</option>
-                                <option value="2025">2025</option>
-                                <option value="2026">2026</option>
-                                <option value="2028">2028</option>
-                                <option value="2029">2029</option>
-                                <option value="2030">2030</option>
-                            </select>
-                            <select class="block w-full p-1 ml-4 text-xs text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" wire:model="month">
-                                <option value="01">Enero</option>
-                                <option value="02">Febrero</option>
-                                <option value="03">Marzo</option>
-                                <option value="04">Abril</option>
-                                <option value="05">Mayo</option>
-                                <option value="06">Junio</option>
-                                <option value="07">Julio</option>
-                                <option value="08">Agosto</option>
-                                <option value="09">Septiembre</option>
-                                <option value="10">Octubre</option>
-                                <option value="11">Noviembre</option>
-                                <option value="12">Diciembre</option>
-                            </select>
+                                <button wire:click="searchByItems" type="button" class="inline-flex items-center px-2 py-1 ml-5 text-sm font-medium text-center text-white rounded-lg bg-sky-700 hover:bg-sky-800 focus:ring-4 focus:outline-none focus:ring-sky-300 dark:bg-sky-600 dark:hover:bg-sky-700 dark:focus:ring-sky-800">Buscar</button>
+                                @livewire('kine.values-kine',['doctor' => $kine])
+                            </div>
 
-                            <button wire:click="searchByItems" type="button" class="inline-flex items-center px-2 py-1 ml-5 text-sm font-medium text-center text-white rounded-lg bg-sky-700 hover:bg-sky-800 focus:ring-4 focus:outline-none focus:ring-sky-300 dark:bg-sky-600 dark:hover:bg-sky-700 dark:focus:ring-sky-800">Buscar</button>
-                            @livewire('kine.values-kine',['doctor' => $kine])
-                        </div>
+                            <div class="p-5 border border-gray-200 rounded-b-xl dark:border-gray-700 dark:bg-gray-900">
+                                <div class="flex">
+                                    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                            <tr class="text-center">
+                                                <th scope="col" class="px-6 py-3">Paciente</th>
+                                                <th scope="col" class="px-6 py-3">Tipo</th>
+                                                <th scope="col" class="px-6 py-3">N°&nbsp;Sesión</th>
+                                                <th scope="col" class="px-6 py-3">Valor&nbsp;Cliente</th>
+                                                <th scope="col" class="px-6 py-3">
+                                                    Valor&nbsp;Kine
+                                                </th>
+                                                <th scope="col" class="px-6 py-3">
+                                                    A&nbsp;Favor
+                                                </th>
+                                                <th scope="col" class="px-6 py-3">
+                                                    Estado
+                                                </th>
+                                                <th colspan="2" class="px-6 py-3">
+                                                    <span class="sr-only">Actions</span>
+                                                </th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            @forelse ($applyItems as $applyItem)
+                                            <tr class="text-center uppercase bg-white border-b dark:border-gray-700 hover:bg-cyan-50">
+                                                <td scope="row" class="px-1 py-1 text-gray-900 font-sm text-['9px'] whitespace-nowrap dark:text-white">
+                                                    {{ $applyItem->application->user->name ?? '' }}
+                                                    {{ $applyItem->application->user->last_name ?? '' }}
+                                                </td class="px-1 py-1">
 
-                        <div class="p-5 border border-gray-200 rounded-b-xl dark:border-gray-700 dark:bg-gray-900">
-                            <div class="flex">
-                                <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                                        <tr class="text-center">
-                                            <th scope="col" class="px-6 py-3">Paciente</th>
-                                            <th scope="col" class="px-6 py-3">Tipo</th>
-                                            <th scope="col" class="px-6 py-3">N°&nbsp;Sesión</th>
-                                            <th scope="col" class="px-6 py-3">Valor&nbsp;Cliente</th>
-                                            <th scope="col" class="px-6 py-3">
-                                                Valor&nbsp;Kine
-                                            </th>
-                                            <th scope="col" class="px-6 py-3">
-                                                A&nbsp;Favor
-                                            </th>
-                                            <th scope="col" class="px-6 py-3">
-                                                Estado
-                                            </th>
-                                            <th colspan="2" class="px-6 py-3">
-                                                <span class="sr-only">Actions</span>
-                                            </th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        @forelse ($applyItems as $applyItem)
-                                        <tr class="text-center uppercase bg-white border-b dark:border-gray-700 hover:bg-cyan-50">
-                                            <td scope="row" class="px-1 py-1 text-gray-900 font-sm text-['9px'] whitespace-nowrap dark:text-white">
-                                                {{ $applyItem->application->user->name ?? '' }}
-                                                {{ $applyItem->application->user->last_name ?? '' }}
-                                            </td class="px-1 py-1">
-
-                                            <td>
-                                                {{ $applyItem->applicationType->name }}
-                                            </td>
-                                            <td class="px-1 py-1">{{ $applyItem->id ?? ''}}</td>
-                                            <td class="px-1 py-1">
-                                                ${{ number_format($applyItem->price,0,',','.') }}.-
-                                            </td>
-                                            <td class="px-1 py-1">
-                                            @if($applyItem->applicationTypeUser)
-                                                ${{ number_format($applyItem->applicationTypeUser->price,0,',','.') ?? '0'}}.-
-                                            @else
-                                                sin datos
-                                            @endif
-                                            </td>
-                                            <td>
-                                        @if($applyItem->applicationTypeUser)
-                                                      ${{ number_format($applyItem->price-$applyItem->applicationTypeUser->price,0,',','.') ?? '0' }}.-
-                                            @else
-                                                sin datos
-                                            @endif
-                                            </td>
-                                            <td class="flex px-1 py-1">
-                                                @if ($applyItem)
-
-                                                @if ($applyItem->status === 0 )
-                                                Pendiente
-                                                @elseif($applyItem->status === 1)
-                                                Atendido
-                                                @endif
-                                                @endif
-                                            </td>
-                                            <td>
-                                            </td>
-                                        </tr>
-                                         @if ($loop->last)
-                                        <tr class="text-center uppercase bg-white border-b dark:border-gray-700 hover:bg-cyan-50">
-                                                <td colspan="6"></td>
                                                 <td>
-                                                    <a href="reporte-pdf/{{ $this->buscarFecha }}/{{$this->kine->id}}" target="_blank" type="button" class="inline-flex items-center px-2 py-1 my-2 ml-5 text-sm font-medium text-center text-white rounded-lg bg-sky-700 hover:bg-sky-800 focus:ring-4 focus:outline-none focus:ring-sky-300 dark:bg-sky-600 dark:hover:bg-sky-700 dark:focus:ring-sky-800">Pdf</a>
+                                                    {{ $applyItem->applicationType->name }}
+                                                </td>
+                                                <td class="px-1 py-1">{{ $applyItem->id ?? ''}}</td>
+                                                <td class="px-1 py-1">
+                                                    ${{ number_format($applyItem->price,0,',','.') }}.-
+                                                </td>
+                                                <td class="px-1 py-1">
+                                                @if($applyItem->applicationTypeUser)
+                                                    ${{ number_format($applyItem->applicationTypeUser->price,0,',','.') ?? '0'}}.-
+                                                @else
+                                                    sin datos
+                                                @endif
+                                                </td>
+                                                <td>
+                                            @if($applyItem->applicationTypeUser)
+                                                        ${{ number_format($applyItem->price-$applyItem->applicationTypeUser->price,0,',','.') ?? '0' }}.-
+                                                @else
+                                                    sin datos
+                                                @endif
+                                                </td>
+                                                <td class="flex px-1 py-1">
+                                                    @if ($applyItem)
+
+                                                    @if ($applyItem->status === 0 )
+                                                    Pendiente
+                                                    @elseif($applyItem->status === 1)
+                                                    Atendido
+                                                    @endif
+                                                    @endif
+                                                </td>
+                                                <td>
                                                 </td>
                                             </tr>
-                                            @endif
-                                        @empty
-                                        <tr class="text-center">
-                                            <td colspan="7" class="my-2">
-                                                Sin Datos
-                                            </td>
-                                        </tr>
-                                       
-                                        @endforelse
-                                    </tbody>
-                                </table>
+                                            @if ($loop->last)
+                                            <tr class="text-center uppercase bg-white border-b dark:border-gray-700 hover:bg-cyan-50">
+                                                    <td colspan="6"></td>
+                                                    <td>
+                                                        <a href="reporte-pdf/{{ $this->buscarFecha }}/{{$this->kine->id}}" target="_blank" type="button" class="inline-flex items-center px-2 py-1 my-2 ml-5 text-sm font-medium text-center text-white rounded-lg bg-sky-700 hover:bg-sky-800 focus:ring-4 focus:outline-none focus:ring-sky-300 dark:bg-sky-600 dark:hover:bg-sky-700 dark:focus:ring-sky-800">Pdf</a>
+                                                    </td>
+                                                </tr>
+                                                @endif
+                                            @empty
+                                            <tr class="text-center">
+                                                <td colspan="7" class="my-2">
+                                                    Sin Datos
+                                                </td>
+                                            </tr>
+                                        
+                                            @endforelse
+                                        </tbody>
+                                    </table>
+                                </div>
                             </div>
-                        </div>
 
+                        </div>
                     </div>
                 </div>
                 <div class="flex items-center pt-5 space-x-4 border-t-2">
