d99ab987d3c88d8ded1b764c1b399af0cfcdd6d1
Andev2000 <javt1981@gmail.com>
2023-08-26 00:53:20 -0400

    iconos

--- resources/views/livewire/layout/sidebar.blade.php
+++ resources/views/livewire/layout/sidebar.blade.php
@@ -6,28 +6,19 @@
         <ul class="space-y-2">
             <li>
                 <a href="{{ route('pacientes') }}" class="flex items-center p-2 text-base font-medium text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                    <svg aria-hidden="true" class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path>
-                        <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path>
-                    </svg>
+                    <img class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" src="{{ asset('icons/usuario.gif') }}" alt="Icono Pacientes">
                     <span class="ml-3">Pacientes</span>
                 </a>
             </li>
             <li>
                 <a href="{{ route('kines') }}" class="flex items-center p-2 text-base font-medium text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                    <svg aria-hidden="true" class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path>
-                        <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path>
-                    </svg>
+                       <img class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" src="{{ asset('icons/medicamento.gif') }}" alt="Icono Kines">
                     <span class="ml-3">Kines</span>
                 </a>
             </li>
             <li>
                 <a href="{{ route('types') }}" class="flex items-center p-2 text-base font-medium text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                    <svg aria-hidden="true" class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path>
-                        <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path>
-                    </svg>
+                      <img class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" src="{{ asset('icons/controlar.gif') }}" alt="Icono Atenciones">
                     <span class="ml-3">Tipo&nbsp;Atenciones</span>
                 </a>
             </li>
@@ -35,10 +26,12 @@
             <li>
                 <form method="POST" action="{{ route('logout') }}">
                 @csrf
+                 
                     <a href="route('logout')" 
                         onclick="event.preventDefault();
                         this.closest('form').submit();" class="flex items-center p-2 text-base font-medium text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                                    {{ __('Cerrar sesión') }}
+                         <img class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" src="{{ asset('icons/cerrar-sesion.gif') }}" alt="Icono Cerrar Sesion">
+                          <span class="ml-3">Cerrar&nbsp;Sesión</span>
                     </a>
                 </form>
             </li>
