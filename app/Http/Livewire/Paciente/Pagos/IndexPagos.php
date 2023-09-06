14ff195be415e7951906a5d6b0f6691f659ef3be
Andev2000 <javt1981@gmail.com>
2023-09-05 23:26:37 -0300

    update payments user ok

--- app/Http/Livewire/Paciente/Pagos/IndexPagos.php
+++ app/Http/Livewire/Paciente/Pagos/IndexPagos.php
@@ -29,11 +29,6 @@ class IndexPagos extends Component
     public function mount(User $paciente){
         $this->paciente = $paciente;
 
-        $users = User::where('user_type','Paciente')->get();
-        foreach($users as $user){
-            $user->payment_status = 1;
-            $user->save();
-        }
     }
 
     public function render()
