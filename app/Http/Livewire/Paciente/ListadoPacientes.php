a08e44f0bde9d4b8ca548de076168ab339353d6a
Andev2000 <javt1981@gmail.com>
2023-09-07 23:49:37 -0300

    new order

--- app/Http/Livewire/Paciente/ListadoPacientes.php
+++ app/Http/Livewire/Paciente/ListadoPacientes.php
@@ -31,12 +31,17 @@ class ListadoPacientes extends Component
 
         $pacientes = User::where('user_type', 'Paciente')
             ->where(function ($query) {
-                $query->where('name', 'like', '%' . $this->search . '%')->orWhere('last_name', 'like', '%' . $this->search . '%')
-                    ->orWhere('updated_at', 'like', '%' . $this->search . '%');
+                $query->where('name', 'like', '%' . $this->search . '%')->orWhere('last_name', 'like', '%' . $this->search . '%');
             })->orderBy($this->sort, $this->direction)->paginate($this->quantity);
 
 
-           
+            foreach($pacientes as $paciente){
+                    if(count($paciente->applications) == 0){
+                        $paciente->updated_at = '2023-08-01 00:00:00';
+                        $paciente->save();
+                    }
+                
+            }
 
         return view('livewire.paciente.listado-pacientes', compact('pacientes'));
     }
