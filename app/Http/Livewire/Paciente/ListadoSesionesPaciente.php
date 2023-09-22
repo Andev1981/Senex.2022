8b5c97ff85f2c42f1b8045ee04fda2b58d4c1026
Andev2000 <javt1981@gmail.com>
2023-09-22 15:19:47 -0300

    nulled

--- app/Http/Livewire/Paciente/ListadoSesionesPaciente.php
+++ app/Http/Livewire/Paciente/ListadoSesionesPaciente.php
@@ -39,7 +39,7 @@ class ListadoSesionesPaciente extends Component
         }
        
         $this->buscarFecha =  $this->year . '-' . $this->month .'-';
-        if($this->selPaciente != null || $this->selKine != null){
+        if($this->selPaciente != "null" || $this->selKine != "null"){
         
             $applyItems = ApplyItem::with('application','patient','doctor')->where('patient_id', $this->selPaciente)->orWhere('doctor_id', $this->selKine)->where('fecha_atencion', 'like', $this->buscarFecha . '%')->orderBy($this->sort, $this->direction)->paginate($this->quantity);
      
