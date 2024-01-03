8beb66b08dfc0cfe99ac8bfc851f5d14a61dd43c
Andev2000 <javt1981@gmail.com>
2024-01-03 10:49:56 -0300

    2024-1

--- app/Http/Livewire/PagosPaciente/IndexPagos.php
+++ app/Http/Livewire/PagosPaciente/IndexPagos.php
@@ -14,7 +14,7 @@ class IndexPagos extends Component
     use WithPagination;
     public $selectedPaciente;
     public $search;
-    protected $listeners = ['success' => 'render','success-paciente' => 'render'];
+    protected $listeners = ['success' => 'render', 'success-paciente' => 'render'];
     protected $queryString = ['search'];
     public $sort = 'payment_status';
     public $direction = 'asc';
@@ -32,22 +32,22 @@ class IndexPagos extends Component
 
         $allPacientes = Patient::with('applyItems')->get();
 
-        foreach($allPacientes as $paciente){
-             $applyItems = ApplyItem::where('patient_id',$paciente->id)->where('status',1)->get();
+        foreach ($allPacientes as $paciente) {
+            $applyItems = ApplyItem::where('patient_id', $paciente->id)->where('status', 1)->get();
             $pendiente = 0;
-            foreach($applyItems as $applyItem){
+            foreach ($applyItems as $applyItem) {
 
-                    if(auth()->user()->id == 1){
+                /*         if(auth()->user()->id == 1){
                        dump($applyItem->payment); 
-                    }
+                    } */
 
-                if( $applyItem->payment ){
-                
-                    if($pendiente == 0){
-                
+                if ($applyItem->payment) {
+
+                    if ($pendiente == 0) {
+
                         $payment = $applyItem->payment;
 
-                        if($payment->status == 1){
+                        if ($payment->status == 1) {
 
                             $pendiente = 1;
                         }
@@ -58,28 +58,28 @@ class IndexPagos extends Component
                     ]);
                 } */
             }
-       
-            
-            if(count($applyItems) > 0){
-                if($pendiente == 1){
+
+
+            if (count($applyItems) > 0) {
+                if ($pendiente == 1) {
                     $paciente->payment_status = 1;
-                }else{
+                } else {
                     $paciente->payment_status = 2;
                 }
-            }else{
-                    $paciente->payment_status = 3;
+            } else {
+                $paciente->payment_status = 3;
             }
-            
+
             $paciente->save();
             $pendiente = 0;
         }
-            
 
-      
+
+
 
         $pacientes = Patient::where(function ($query) {
-                $query->where('name', 'like', '%' . $this->search . '%')->orWhere('last_name', 'like', '%' . $this->search . '%');
-            })->orderBy($this->sort, $this->direction)->paginate($this->quantity);
+            $query->where('name', 'like', '%' . $this->search . '%')->orWhere('last_name', 'like', '%' . $this->search . '%');
+        })->orderBy($this->sort, $this->direction)->paginate($this->quantity);
 
         return view('livewire.pagos-paciente.index-pagos', compact('pacientes'));
     }
@@ -98,13 +98,15 @@ class IndexPagos extends Component
         }
     }
 
-    public function openDeleteModal($paciente){
-        $this->selectedPaciente ='';
+    public function openDeleteModal($paciente)
+    {
+        $this->selectedPaciente = '';
         $this->selectedPaciente = $paciente;
         $this->openDelPaciente = '';
     }
 
-    public function deletePaciente(){
+    public function deletePaciente()
+    {
         $pacienteDel = Patient::find($this->selectedPaciente['id']);
         $pacienteDel->delete();
         $this->openDelPaciente = 'hidden';
