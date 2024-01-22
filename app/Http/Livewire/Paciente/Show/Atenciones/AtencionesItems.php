367ecfc9f4c996f4c68d4702f2ebd8678fff8e3c
Andev2000 <javt1981@gmail.com>
2024-01-22 03:43:11 -0300

    asdasd

--- app/Http/Livewire/Paciente/Show/Atenciones/AtencionesItems.php
+++ app/Http/Livewire/Paciente/Show/Atenciones/AtencionesItems.php
@@ -34,6 +34,8 @@ class AtencionesItems extends Component
     public function mount(Application $application)
     {
         $this->application = $application;
+        dump('Aplication 1: ', $this->application);
+        dump('User 2: ', $this->application->user);
         $this->paciente = $this->application->user;
     }
 
