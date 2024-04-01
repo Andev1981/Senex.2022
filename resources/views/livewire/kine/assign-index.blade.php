e7c069a78edca53269cc61ce8d58e0b0720cded2
Andev2000 <javt1981@gmail.com>
2024-04-01 12:28:32 -0300

    add fecha table header

--- resources/views/livewire/kine/assign-index.blade.php
+++ resources/views/livewire/kine/assign-index.blade.php
@@ -106,6 +106,7 @@
                                         class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                                         <tr class="text-center">
                                             <th scope="col" class="px-6 py-3">Paciente</th>
+                                            <th scope="col" class="px-6 py-3">Fecha</th>
                                             <th scope="col" class="px-6 py-3">Tipo</th>
                                             <th scope="col" class="px-6 py-3">N°&nbsp;Sesión</th>
                                             <th scope="col" class="px-6 py-3">Valor&nbsp;Cliente</th>
