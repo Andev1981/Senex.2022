77f5b858cafeb305d0c232e11ef0abc1e37c7d43
Andev2000 <javt1981@gmail.com>
2024-04-01 12:40:06 -0300

    update orderBy ok

--- app/Http/Livewire/Kine/AssignIndex.php
+++ app/Http/Livewire/Kine/AssignIndex.php
@@ -81,14 +81,13 @@ class AssignIndex extends Component
 		$this->buscarFecha =  $this->year . '-' . $this->month;
 
 		if ($this->selPaciente != 0) {
-			$applyItems = ApplyItem::with(['patient' => function ($query) {
+			$this->applyItems = ApplyItem::with(['patient' => function ($query) {
 				$query->orderBy('name', 'asc');
 			}], 'application', 'doctor')
 				->where('doctor_id', $this->kine->id)
 				->where('patient_id', $this->selPaciente)
 				->where('status', 1)
 				->where('fecha_atencion', 'like', $this->buscarFecha . '%')->get();
-			$this->applyItems = $applyItems->orderBy('name', 'asc');
 		} else {
 			$this->applyItems = ApplyItem::with(['patient' => function ($query) {
 				$query->orderBy('name', 'asc');
