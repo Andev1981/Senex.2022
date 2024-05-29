68980b2ee0c7c16f883d432e3be652f3a91418fc
Andev2000 <javt1981@gmail.com>
2024-05-29 18:34:55 -0400

    fix

--- resources/views/livewire/pagos-paciente/index-pagos.blade.php
+++ resources/views/livewire/pagos-paciente/index-pagos.blade.php
@@ -201,7 +201,7 @@
                                     @else
                                     <span class="inline-flex items-center bg-red-100 text-red-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-red-900 dark:text-red-300">
                                         <span class="w-2 h-2 mr-1 bg-red-500 rounded-full"></span>
-                                        -------
+                                        Sin Atenciones
                                     </span>
                                     @endif
                                 </td>
@@ -278,8 +278,7 @@
                         <svg aria-hidden="true" class="mx-auto mb-4 text-gray-400 w-14 h-14 dark:text-gray-200" fill="none" stroke="currentColor" viewbox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                         </svg>
-                        <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Seguro que desea eliminar al
-                            el registro?</h3>
+                        <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Seguro que desea desactivara este paciente?</h3>
                         @if ($selectedPaciente)
                         <button type="button" wire:click="deletePaciente()" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">Si,
                             Estoy Seguro</button>
