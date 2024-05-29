21154e8da6db07a1bb406d5178856fc6053e1503
Andev2000 <javt1981@gmail.com>
2024-05-29 01:03:16 -0400

    activar paciente funcionalidad

--- resources/views/livewire/pagos-paciente/index-pagos.blade.php
+++ resources/views/livewire/pagos-paciente/index-pagos.blade.php
@@ -295,7 +295,7 @@
                         </svg>
                         <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Seguro desea habilitar a usuario?</h3>
         
-                        <button type="button" wire:click="deletePaciente()" class="text-white bg-green-600 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">Si,
+                        <button type="button" wire:click="activarPaciente()" class="text-white bg-green-600 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">Si,
                             Estoy Seguro</button>
 
 
