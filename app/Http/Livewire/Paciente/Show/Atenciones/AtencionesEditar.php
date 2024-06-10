d3929e44ba5245ff4ccd57e2a63a7f0b19582346
Andev2000 <javt1981@gmail.com>
2024-06-10 18:28:58 -0400

    e

--- app/Http/Livewire/Paciente/Show/Atenciones/AtencionesEditar.php
+++ app/Http/Livewire/Paciente/Show/Atenciones/AtencionesEditar.php
@@ -38,13 +38,13 @@ class AtencionesEditar extends Component
 
     public function mount($application)
     {
-        dump($application);
-        /*       $this->application = $application;
+
+        $this->application = $application;
         $this->paciente = $application->patient;
         $this->status = $application->status;
         $this->lugar_derivacion = $application->desde;
         $this->profesional_derivacion = $application->derivado;
-        $this->forma_de_pago = $application->type_payment; */
+        $this->forma_de_pago = $application->type_payment;
     }
 
     public function saveAtencion()
