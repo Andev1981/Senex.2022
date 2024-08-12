929dc457a87f1ab6e38554834955f228418b10d1
Andev2000 <javt1981@gmail.com>
2024-08-12 11:29:25 -0400

    listado reseteado

--- app/Http/Livewire/PagosPaciente/ListadoAtenciones.php
+++ app/Http/Livewire/PagosPaciente/ListadoAtenciones.php
@@ -15,18 +15,23 @@ class ListadoAtenciones extends Component
         $year,
         $search,
         $sort = 'updated_at',
-        $direction = 'desc';
+        $direction = 'desc',
+        $monthName = '',
+        $reloadStatus = 0;
 
     protected $queryString = ['search'];
 
     public function render()
     {
-        $this->buscarFecha = Carbon::now();
-        $this->month = $this->buscarFecha->format('m');
-        $this->year = $this->buscarFecha->format('Y');
+        if ($this->reloadStatus == 0) {
+            $this->buscarFecha = Carbon::now();
+            $this->month = $this->buscarFecha->format('m');
+            $this->year = $this->buscarFecha->format('Y');
+            $this->reloadStatus = 1;
+        }
         $this->buscarFecha =  $this->year . '-' . $this->month . '-';
 
-        $applyItems = ApplyItem::where('status', 1)->where('updated_at', 'like', $this->buscarFecha . '%')->orderBy($this->sort, $this->direction)->paginate('10');
+        $applyItems = ApplyItem::where('estado_pago', 1)->where('updated_at', 'like', $this->buscarFecha . '%')->orderBy($this->sort, $this->direction)->paginate('30');
 
 
         return view('livewire.pagos-paciente.listado-atenciones', compact('applyItems'));
@@ -43,6 +48,51 @@ class ListadoAtenciones extends Component
             }
         } else {
             $this->sort = $sort;
+        }
+    }
+
+    public function setMonth($month)
+    {
+        switch ($month) {
+            case '01':
+                $this->monthName = 'Enero';
+                break;
+            case '02':
+                $this->monthName = 'Febrero';
+                break;
+            case '03':
+                $this->monthName = 'Marzo';
+                break;
+            case '04':
+                $this->monthName = 'Abril';
+                break;
+            case '05':
+                $this->monthName = 'Mayo';
+                break;
+            case '06':
+                $this->monthName = 'Junio';
+                break;
+            case '07':
+                $this->monthName = 'Julio';
+                break;
+            case '08':
+                $this->monthName = 'Agosto';
+                break;
+            case '09':
+                $this->monthName = 'Septiembre';
+                break;
+            case '10':
+                $this->monthName = 'Octubre';
+                break;
+            case '11':
+                $this->monthName = 'Noviembre';
+                break;
+            case '12':
+                $this->monthName = 'Diciembre';
+                break;
+            default:
+                $this->monthName = '';
+                break;
         }
     }
 }
