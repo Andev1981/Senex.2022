28fed9566f5b784bf5aeb7c7e93ca5e0afebc806
Andev2000 <javt1981@gmail.com>
2024-10-02 16:40:34 -0300

    fix controller pdf

--- app/Http/Controllers/ReportePdfController.php
+++ app/Http/Controllers/ReportePdfController.php
@@ -27,13 +27,12 @@ class ReportePdfController extends Controller
         // Obtener los datos
         $applyItems = ApplyItem::with(['patient' => function ($query) {
             $query->orderBy('name', 'asc');
-        }], 'assign')->where('fecha_atencion', 'like', $buscarFecha . '%')->where('status', 1)->where('doctor_id', $kine)->orderByDesc(function ($query) {
+        }], 'assign')->where('fecha_atencion', 'like', $buscarFecha)->where('status', 1)->where('doctor_id', $kine)->orderByDesc(function ($query) {
             $query->from('patients')
                 ->whereColumn('patients.id', '=', 'apply_items.patient_id')
                 ->select('name')
                 ->limit(1);
         })->orderBy('fecha_atencion', 'asc')->get();
-
 
         $nameUser = $applyItems[0]->doctor->name . ' ' . $applyItems[0]->doctor->last_name;
         $total = $applyItems[0]->sum('price');
