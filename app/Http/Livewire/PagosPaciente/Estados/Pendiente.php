c4d4e7e0de8e78aa9b4fdda0be8959170cf5b739
Andev2000 <javt1981@gmail.com>
2024-11-18 19:52:20 -0300

    aaa

--- app/Http/Livewire/PagosPaciente/Estados/Pendiente.php
+++ app/Http/Livewire/PagosPaciente/Estados/Pendiente.php
@@ -34,7 +34,7 @@ class Pendiente extends Component
         $this->fechaBuscar = $this->fechaActual->format('Y-m');
         $pacientes = Patient::where(function ($query) {
             $query->where('name', 'like', '%' . $this->search . '%')->orWhere('last_name', 'like', '%' . $this->search . '%');
-        })->where('status', 1)->where('payment_status', 1)->where('payment_status', 3)->orderBy($this->sort, $this->direction)->paginate($this->quantity);
+        })->where('status', 1)->where('payment_status', 1)->orderBy($this->sort, $this->direction)->paginate($this->quantity);
         return view('livewire.pagos-paciente.estados.pendiente', compact('pacientes'));
     }
 
