a310bf63582894201deaa12ff2f655f50da24656
Andev2000 <javt1981@gmail.com>
2024-11-23 16:00:43 -0300

    mes

--- app/Http/Controllers/ReportePdfController.php
+++ app/Http/Controllers/ReportePdfController.php
@@ -90,7 +90,7 @@ class ReportePdfController extends Controller
         }
         $total = $applyItems[0]->sum('price');
         $fechaString = Carbon::parse($applyItems[0]->fecha_atencion);
-        $fecha = $fechaString->format('MM-Y');
+        $fecha = $fechaString->format('m-Y');
 
         //dd($total, $applyItems, $fecha, $this->totalKine, $this->totalPacientes);
 
