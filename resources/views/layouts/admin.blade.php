95cbbd0d4713b4ea43720eaf5bbaf49b8e859ebe
Andev2000 <javt1981@gmail.com>
2025-07-28 15:28:35 -0400

    add jsx

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -7,7 +7,7 @@
 
         <title>{{ config('app.name', 'Senex-App') }}</title>
 
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @vite(['resources/css/app.css', 'resources/js/app.jsx'])
 
     </head>
     <body class="font-sans antialiased">
@@ -17,7 +17,7 @@
             @isset($header)
                  <!-- Page Heading -->
             <header class="bg-white shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                <div class="px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
                     {{ $header }}
                 </div>
             </header>
