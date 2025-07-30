cc078e3786e6d3e159c5aec207809c457a26fb29
Andev2000 <javt1981@gmail.com>
2025-07-30 00:34:03 -0400

    consultas ok

--- app/Http/Livewire/Paciente/Show/Atenciones/EditarItem.php
+++ app/Http/Livewire/Paciente/Show/Atenciones/EditarItem.php
@@ -15,22 +15,22 @@ use Livewire\Component;
 class EditarItem extends Component
 {
     public ApplyItem $applyItem;
-    public  $patient, 
-            $types = [],
-            $kines = [],
-            $openItem = 'hidden',
-            $openDelItem = 'hidden',
-            $selectedKine,
-            $selectedStatus,
-            $selectedType,
-            $application,
-            $countApplies,
-            $applypaciente,
-            $fecha_atencion,
-            $comments = '',
-            $price,
-            $numero_sesion,
-            $errorNumSesion=false;
+    public  $patient,
+        $types = [],
+        $kines = [],
+        $openItem = 'hidden',
+        $openDelItem = 'hidden',
+        $selectedKine,
+        $selectedStatus,
+        $selectedType,
+        $application,
+        $countApplies,
+        $applypaciente,
+        $fecha_atencion,
+        $comments = '',
+        $price,
+        $numero_sesion,
+        $errorNumSesion = false;
 
     protected $rules = [
         'selectedKine' => 'required',
@@ -47,41 +47,44 @@ class EditarItem extends Component
         return view('livewire.paciente.show.atenciones.editar-item');
     }
 
-    public function mount(ApplyItem $applyItem){
+    public function mount(ApplyItem $applyItem)
+    {
 
-        $this->applyItem = $applyItem;      
+
+
+        $this->applyItem = $applyItem;
         $this->application = $applyItem->application;
         $this->patient = $this->applyItem->patient;
-        if($applyItem->doctor){
+        if ($applyItem->doctor) {
             $this->selectedKine = $applyItem->doctor->id;
-        }else{
+        } else {
             $this->selectedKine = 1;
         }
         $this->selectedStatus = $applyItem->status;
-        if($applyItem->fecha_atencion){
-        $this->fecha_atencion = Carbon::parse(strtotime($applyItem->fecha_atencion))->format('Y-m-d');
+        if ($applyItem->fecha_atencion) {
+            $this->fecha_atencion = Carbon::parse(strtotime($applyItem->fecha_atencion))->format('Y-m-d');
         }
         $this->comments = $applyItem->comments;
         $this->selectedType = $applyItem->application_type_id;
         $this->price = $applyItem->price;
         $this->numero_sesion = $applyItem->numero_sesion;
-        $this->countApplies = ApplyItem::where('application_id',$this->application->id)->count();
-        $this->kines = Doctor::all();
-        $this->types = ApplicationType::all();
-
+        $this->countApplies = ApplyItem::where('application_id', $this->application->id)->count();
+        $this->kines = Doctor::select('id', 'name', 'last_name')->get();
+        $this->types = ApplicationType::select('id', 'name',)->get();
     }
 
-    public function save(){
+    public function save()
+    {
 
 
-        $this->validate();   
-        
+        $this->validate();
+
 
         $this->applyItem->user_id = $this->selectedKine;
         $this->applyItem->doctor_id = $this->selectedKine;
         $this->applyItem->status = $this->selectedStatus;
 
-        if($this->fecha_atencion){
+        if ($this->fecha_atencion) {
             $this->applyItem->fecha_atencion = $this->fecha_atencion;
         }
 
@@ -91,15 +94,16 @@ class EditarItem extends Component
         $this->applyItem->numero_sesion = $this->numero_sesion;
         $this->applyItem->save();
         $this->clear();
-        
     }
 
-    public function delete(){
+    public function delete()
+    {
         $this->applyItem->delete();
         $this->clear();
     }
 
-    public function clear(){
+    public function clear()
+    {
         $this->resetValidation();
         $this->resetErrorBag();
         $this->dispatchBrowserEvent('swal-success');
