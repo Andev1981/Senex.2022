2603578527d7ad12a6310f33ffab35007989dd91
Andev2000 <javt1981@gmail.com>
2025-07-30 01:26:13 -0400

    ahora si

---
+++ public/build/assets/app-577560de.js
@@ -0,0 +1,1 @@
+import{c as e}from"./index-e9d17dae.js";window.createPopper=e;
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -7,7 +7,7 @@
 
         <title>{{ config('app.name', 'Senex-App') }}</title>
 
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @vite(['resources/css/app.css'])
 
 
     </head>
