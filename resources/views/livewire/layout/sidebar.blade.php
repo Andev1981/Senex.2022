14b8fe6b59ec488c37c0a9b91c8a5ed80c91e727
Andev2000 <javt1981@gmail.com>
2025-07-30 20:35:33 -0400

    fix route

--- resources/views/livewire/layout/sidebar.blade.php
+++ resources/views/livewire/layout/sidebar.blade.php
@@ -4,7 +4,7 @@
 	<div class="h-full px-3 py-5 overflow-y-auto bg-white dark:bg-gray-800">
 		<ul class="space-y-2">
 			<li>
-				<a href="{{ route('listado-pacientes') }}"
+				<a href="{{ route('listado.pacientes') }}"
 					class="flex items-center p-2 text-base font-medium text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group {{ Request::path() == 'listado-pacientes' ? 'border-2 border-sky-600' : ''}}">
 					<img
 						class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
