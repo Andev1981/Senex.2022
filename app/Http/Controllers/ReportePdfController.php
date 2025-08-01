64103fc7810fc6fd638106205bb803a79c4c55da
Andev2000 <javt1981@gmail.com>
2025-08-01 17:32:42 -0400

    chunk 350

--- app/Http/Controllers/ReportePdfController.php
+++ app/Http/Controllers/ReportePdfController.php
@@ -91,7 +91,7 @@ class ReportePdfController extends Controller
         }
 
         // Dividir en partes de 100
-        $chunks = $applyItems->chunk(500);
+        $chunks = $applyItems->chunk(350);
         $chunkIndex = 1;
         $pdfFiles = [];
 
