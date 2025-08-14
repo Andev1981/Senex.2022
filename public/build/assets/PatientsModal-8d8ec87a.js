4cc8906470149f33e4df0bccbbfc80528f03b878
Andev2000 <javt1981@gmail.com>
2025-08-14 18:19:42 -0400

    npm run build

--- public/build/assets/PatientsModal-8d8ec87a.js
+++
@@ -1,1 +0,0 @@
-import{a as o}from"./app-40003c6c.js";import"./index-7ac5b536.js";function d({patient:r}){return o("div",{children:(r==null?void 0:r.name)+" "+(r==null?void 0:r.last_name)})}export{d as default};
--- public/build/assets/SesionesModal-13b36632.js
+++
@@ -1,1 +0,0 @@
-import{a as o}from"./app-40003c6c.js";import"./index-7ac5b536.js";function n({sesion:e}){return o("div",{children:"Sesion 1"})}export{n as default};
