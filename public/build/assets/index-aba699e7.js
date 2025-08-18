7de14033458a0d34ba9c67679e25bcfc73ecc983
Andev2000 <javt1981@gmail.com>
2025-08-18 19:27:19 -0400

    npm run build

--- public/build/assets/index-aba699e7.js
+++ public/build/assets/index-aba699e7.js
@@ -1,4 +1,4 @@
-import{c as re}from"./createLucideIcon-c37e0edc.js";import{r as j}from"./app-f834fcfb.js";const dt=re("ChevronDown",[["path",{d:"m6 9 6 6 6-6",key:"qrunsl"}]]),ft=re("ChevronUp",[["path",{d:"m18 15-6-6-6 6",key:"153udz"}]]);/**
+import{c as re}from"./createLucideIcon-c8e5e930.js";import{r as j}from"./app-bf370430.js";const dt=re("ChevronDown",[["path",{d:"m6 9 6 6 6-6",key:"qrunsl"}]]),ft=re("ChevronUp",[["path",{d:"m18 15-6-6-6 6",key:"153udz"}]]);/**
    * table-core
    *
    * Copyright (c) TanStack
